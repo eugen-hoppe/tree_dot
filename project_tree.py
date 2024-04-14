061a2bb361918595d175275ee690e21e34fddd87
Eugen <eugen.hoppe.de@gmail.com>
2024-04-15 01:19:40 +0200

    overview

--- project_tree.py
+++ project_tree.py
@@ -2,10 +2,21 @@ import os
 from pprint import pprint
 
 
-ROOT_DIR = "."
-EXTENSIONS = ['py', "txt"]
-DIR_PREFIXES = ['.', '__']
-FILE_PREFIXES = ['.', 'log_', "treedot"]
+ROOT_DIR = "sandbox"
+EXTENSIONS = [
+    "py",
+    "html",
+    "txt",
+]
+DIR_PREFIXES = [
+    ".",
+    "__",
+]
+FILE_PREFIXES = [
+    ".",
+    "log_",
+    "project_tree"
+]
 
 
 def create_project_tree(
@@ -26,16 +37,42 @@ def create_project_tree(
         elif os.path.isfile(path):
             if any(item.startswith(prefix) for prefix in file_prefixes):
                 continue
-            if path.split('.')[-1] in extensions:
+            if path.split(".")[-1] in extensions:
                 result[file_count] = item
                 file_count += 1
     return result
 
 
+def read_files_to_dict(
+    directory: str,
+    file_dict: dict[str | int, str | dict],
+    result: dict[str, str]
+) -> None:
+    for key, value in file_dict.items():
+        if isinstance(key, int):
+            file_path = os.path.join(directory, value)
+            try:
+                with open(file_path, "r", encoding="utf-8") as file:
+                    result[file_path] = file.read()
+            except Exception as e:
+                result[file_path] = str(e)
+        elif isinstance(value, dict):
+            new_directory = os.path.join(directory, key)
+            read_files_to_dict(new_directory, value, result)
 
-project_dict = create_project_tree(
-    ROOT_DIR, EXTENSIONS, DIR_PREFIXES, FILE_PREFIXES
-)
+
+def create_content_dict(root_directory, project_dict):
+    content_dict = {}
+    read_files_to_dict(root_directory, project_dict, content_dict)
+    return content_dict
 
 
-pprint(project_dict, indent=4)
+if __name__ == "__main__":
+    project_dict = create_project_tree(
+        ROOT_DIR, EXTENSIONS, DIR_PREFIXES, FILE_PREFIXES
+    )
+    content_dict = create_content_dict(ROOT_DIR, project_dict)
+
+    pprint(project_dict, indent=4)
+    print()
+    pprint(content_dict, indent=4)
