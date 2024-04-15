a75401dafd6bbca39fd78274bc5e6f378ec6165b
Eugen <eugen.hoppe.de@gmail.com>
2024-04-15 13:48:33 +0200

    overview script

--- project_tree.py
+++ project_tree.py
@@ -1,29 +1,51 @@
 import os
-from pprint import pprint
+
+from datetime import datetime
+from typing import TextIO
 
 
+# Configuration
+# =============
 ROOT_DIR = "sandbox"
-EXTENSIONS = [
+OVERVIEW_PATH = ".overview"
+INCLUDE_EXTENSIONS = [
     "py",
     "html",
     "txt",
 ]
-DIR_PREFIXES = [
+EXCLUDE_DIR_PREFIXES = [
     ".",
     "__",
 ]
-FILE_PREFIXES = [
-    os.path.basename(__file__).removesuffix(".py"),  # this file
+EXCLUDE_FILE_PREFIXES = [
     ".",
     "log_",
 ]
 
 
+# Markdown Creation
+# =================
+PROJECT_TREE_TITLE = "Project Structure"
+CONTENT_SECTION_TITLE = "Content"
+CODE_BLOCK_LABELS = {
+    "py": "python",
+    "html": "html",
+    "txt": "txt",
+}
+
+# Application Constants
+# =====================
+EXCLUDE_FILE_PREFIXES_INCLUDING_THIS_FILE = [
+    os.path.basename(__file__).removesuffix(".py"),
+    *EXCLUDE_FILE_PREFIXES
+]
+
+
 def create_project_tree(
     directory: str,
-    extensions: str,
-    dir_prefixes: list[str],
-    file_prefixes: list[str]
+    extensions: str,  # include
+    dir_prefixes: list[str],  # exclude
+    file_prefixes: list[str]  # exclude
 ) -> dict[str | int, str | dict]:
     result = {}
     dir_content = sorted(os.listdir(directory))
@@ -72,12 +94,62 @@ def create_content_dict(
     return content_dict
 
 
+def write_tree_to_md(
+    dict_item: dict[str | int, str | dict],
+    md_file: TextIO,
+    indent=''
+) -> None:
+    for key, value in dict_item.items():
+        if isinstance(key, int):
+            md_file.write(f"{indent}- {value}\n")
+        else:
+            md_file.write(f"{indent}- {key}/\n")
+            write_tree_to_md(value, md_file, indent + '  ')
+
+
+def generate_markdown_overview(
+    project_dict: dict[str | int, str | dict],
+    content_dict: dict[str, str],
+    output_path: str | None = None,
+    h2: str = "## ",
+    h3: str ="### ",
+    br: str ="\n",
+    code: str = "```"
+) -> None:
+    if output_path is None:
+        output_path = os.path.join(
+            OVERVIEW_PATH, f"{datetime.now().strftime('%Y%m%d-%H%M%S')}.md"
+        )
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    with open(output_path, 'w', encoding='utf-8') as md_file:
+
+        # Project Tree Block
+        # ==================
+        md_file.write(f"{h2}{PROJECT_TREE_TITLE}{br}{br}")
+        md_file.write(f"{code}txt{br}")
+        write_tree_to_md(project_dict, md_file)
+        md_file.write(f"{br}{code}{br}{br}")
+        md_file.write(f"{h2}Dateiinhalte{br}{br}")
+
+        for path, content in content_dict.items():
+
+            # Code Blocks
+            # ===========
+            code_label = CODE_BLOCK_LABELS[path.split("/")[-1].split(".")[-1]]
+            md_file.write(f"{h3}{path}{br}{br}")
+            md_file.write(f"{code}{code_label}{br}")
+            md_file.write(content)
+            md_file.write(f"{br}{code}{br}{br}")
+
+
 if __name__ == "__main__":
     project_dict = create_project_tree(
-        ROOT_DIR, EXTENSIONS, DIR_PREFIXES, FILE_PREFIXES
+        ROOT_DIR,
+        INCLUDE_EXTENSIONS,
+        EXCLUDE_DIR_PREFIXES,
+        EXCLUDE_FILE_PREFIXES
     )
-    content_dict = create_content_dict(ROOT_DIR, project_dict)
-
-    pprint(project_dict, indent=4)
-    print()
-    pprint(content_dict, indent=4)
+    generate_markdown_overview(
+        project_dict,
+        content_dict=create_content_dict(ROOT_DIR, project_dict)
+    )
