ebb0d26eeca6d87fcec50ec35e8847a538b15f14
Eugen <eugen.hoppe.de@gmail.com>
2024-04-22 20:57:51 +0200

    mention view settings root 2

--- dot.py
+++ dot.py
@@ -166,7 +166,7 @@ def generate_markdown_overview(
         md_file.write(f"{br}{h2}File and Directories Considerations{br}{br}")
         md_file.write(f"{code}txt{br}")
         md_file.write(
-            f"ROOT_DIR: ({', '.join(ROOT_DIR)}){br}"
+            f"ROOT_DIR: {ROOT_DIR}{br}"
             + f"INCLUDE_EXTENSIONS: ({', '.join(INCLUDE_EXTENSIONS)}){br}"
             + f"EXCLUDE_DIR_PREFIXES: ({', '.join(EXCLUDE_DIR_PREFIXES)}){br}"
             + f"EXCLUDE_FILE_PREFIXES: ({', '.join(EXCLUDE_FILE_PREFIXES)}){br}"
